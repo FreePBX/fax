d3386eeb3cc374e61e8580a0d2efbdd28d51e775
Andrew Nagy <andrew.nagy@the159.com>
2015-06-09 15:44:51 -0400

    quick create styling changes

--- views/quickCreate.php
+++ views/quickCreate.php
@@ -1,10 +1,27 @@
-<div class="form-group">
-	<label for="faxenabled"><?php echo _('Enable Fax')?> <i class="fa fa-question-circle fpbx-help-icon" data-for="faxenabled"></i></label><br/>
-	<span class="radioset">
-		<input type="radio" name="faxenabled" id="fm_on" value="yes">
-		<label for="fm_on"><?php echo _('Yes')?></label>
-		<input type="radio" name="faxenabled" id="fm_off" value="no" checked>
-		<label for="fm_off"><?php echo _('No')?></label>
-	</span>
-	<span id="faxenabled-help" class="help-block fpbx-help-block"><?php echo _('Enable this user to receive faxes')?></span>
+<div class="element-container">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-3">
+						<label class="control-label" for="faxenabled"><?php echo _('Enable Fax')?></label>
+						<i class="fa fa-question-circle fpbx-help-icon" data-for="faxenabled"></i>
+					</div>
+					<div class="col-md-9">
+						<span class="radioset">
+							<input type="radio" name="faxenabled" id="fm_on" value="yes">
+							<label for="fm_on"><?php echo _('Yes')?></label>
+							<input type="radio" name="faxenabled" id="fm_off" value="no" checked>
+							<label for="fm_off"><?php echo _('No')?></label>
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<span id="faxenabled-help" class="help-block fpbx-help-block"><?php echo _('Enable this user to receive faxes')?></span>
+		</div>
+	</div>
 </div>
