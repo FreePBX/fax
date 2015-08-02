3c5eecaee08e1119d8b0b3c5e281da09fd08a982
Rob Thomas <xrobau@gmail.com>
2015-08-02 14:59:46 -0700

    Remove unneeded divs

--- views/quickCreate.php
+++ views/quickCreate.php
@@ -1,20 +1,16 @@
 <div class="element-container">
 	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="form-group">
-					<div class="col-md-3">
-						<label class="control-label" for="faxenabled"><?php echo _('Enable Fax')?></label>
-					</div>
-					<div class="col-md-9">
-						<span class="radioset">
-							<input type="radio" name="faxenabled" id="fax_on" value="yes">
-							<label for="fm_on"><?php echo _('Yes')?></label>
-							<input type="radio" name="faxenabled" id="fax_off" value="no" checked>
-							<label for="fm_off"><?php echo _('No')?></label>
-						</span>
-					</div>
-				</div>
+		<div class="form-group">
+			<div class="col-md-3">
+				<label class="control-label" for="faxenabled"><?php echo _('Enable Fax')?></label>
+			</div>
+			<div class="col-md-9">
+				<span class="radioset">
+					<input type="radio" name="faxenabled" id="fax_on" value="yes">
+					<label for="fm_on"><?php echo _('Yes')?></label>
+					<input type="radio" name="faxenabled" id="fax_off" value="no" checked>
+					<label for="fm_off"><?php echo _('No')?></label>
+				</span>
 			</div>
 		</div>
 	</div>
