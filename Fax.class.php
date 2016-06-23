9e2457fad91b1f6783123b93cd449d71c9274558
Andrew Nagy <andrew.nagy@the159.com>
2016-06-22 17:12:42 -0700

    FREEPBX-12599 Legacy Email box cant see title

--- Fax.class.php
+++ Fax.class.php
@@ -573,7 +573,7 @@ class Fax extends \FreePBX_Helpers implements \BMO {
 								<div class="row">
 									<div class="form-group">
 										<div class="col-md-3">
-											<label class="control-label" for="legacy_email"><?php echo _("Fax Email Destination") ?></label>
+											<label class="control-label" for="legacy_email">'._("Fax Email Destination").'</label>
 											<i class="fa fa-question-circle fpbx-help-icon" data-for="legacy_email"></i>
 										</div>
 										<div class="col-md-9">
