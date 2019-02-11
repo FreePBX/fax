7e29f2a83a14923553b4945f4f056c86b1cf77c4
Andrew Nagy <andrew.nagy@the159.com>
2019-02-11 13:24:04 -0500

    [Module Tag script: fax 15.0.9] backup and restore

--- Restore.php
+++ Restore.php
@@ -2,32 +2,32 @@
 namespace FreePBX\modules\Fax;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = reset($this->getConfigs());
-    $this->processConfigs($configs);
-  }
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir)
-  {
-    $tables = array_flip($tables + $unknownTables);
-    if (!isset($tables['fax_details'])) {
-      return $this;
-    }
-    $cb = $this->FreePBX->Fax;
-    $cb->setDatabase($pdo);
-    $configs = [
-      'incoming' => $cb->getIncoming(),
-      'users' => $cb->listUsers(),
-    ];
-    $this->processConfigs($configs);
-    return $this;
-  }
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		$this->processConfigs($configs);
+	}
+	public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir)
+	{
+		$tables = array_flip($tables + $unknownTables);
+		if (!isset($tables['fax_details'])) {
+			return $this;
+		}
+		$cb = $this->FreePBX->Fax;
+		$cb->setDatabase($pdo);
+		$configs = [
+			'incoming' => $cb->getIncoming(),
+			'users' => $cb->listUsers(),
+		];
+		$this->processConfigs($configs);
+		return $this;
+	}
 
-  public function processConfigs($configs){
-    foreach ($configs['users'] as $user) {
-      $this->FreePBX->Fax->saveUser($user['faxext'], $user['faxenabled'], $user['faxemail'], $user['faxattachformat']);
-    }
-    foreach ($configs['incoming'] as $incoming) {
-      $this->FreePBX->Fax->saveIncoming($incoming['cidnum'], $incoming['extension'], $incoming['enabled'], $incoming['detection'], $incoming['detectionwait'], $incoming['destination'], $incoming['legacy_email'], $incoming['ring']);
-    }
-  }
+	public function processConfigs($configs){
+		foreach ($configs['users'] as $user) {
+			$this->FreePBX->Fax->saveUser($user['faxext'], $user['faxenabled'], $user['faxemail'], $user['faxattachformat']);
+		}
+		foreach ($configs['incoming'] as $incoming) {
+			$this->FreePBX->Fax->saveIncoming($incoming['cidnum'], $incoming['extension'], $incoming['enabled'], $incoming['detection'], $incoming['detectionwait'], $incoming['destination'], $incoming['legacy_email'], $incoming['ring']);
+		}
+	}
 }
